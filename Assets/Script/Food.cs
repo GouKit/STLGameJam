1ff044c544ebb53486f0d8857db4cfa7f22121d9
Jin-Hwan Kim <46664980+GouGit@users.noreply.github.com>
2019-10-05 03:12:20 +0900

    file clear

--- Assets/Script/Food.cs
+++ Assets/Script/Food.cs
@@ -4,16 +4,19 @@ using UnityEngine;
 
 public class Food : MonoBehaviour
 {
+    private Collider2D coll;
     private Vector3 origin;
 
     void Start()
     {
+        coll = GetComponent<Collider2D>();
         origin = transform.position;
     }
 
     void OnMouseDown()
     {
-        transform.localScale *= 1.5f;    
+        transform.localScale *= 1.5f;
+        coll.enabled = false;    
     }
 
     void OnMouseDrag()
@@ -25,7 +28,13 @@ public class Food : MonoBehaviour
 
     void OnMouseUp()
     {
+        Vector2 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         transform.localScale = new Vector3(1,1,1);
         transform.position = origin;
+
+        GameObject temp = Physics2D.OverlapBox(new Vector2(pos.x, pos.y), new Vector2(1,1), 0).gameObject;
+        Debug.Log(temp.name);
+
+        coll.enabled = true;
     }    
 }
