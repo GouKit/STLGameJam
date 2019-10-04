ac6ad61f02e287d622aec9b5da45852b14e9e716
Jin-Hwan Kim <46664980+GouGit@users.noreply.github.com>
2019-10-05 01:08:29 +0900

    object

---
+++ Assets/JinHwan/Script/Food.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Food : MonoBehaviour
+{
+    private Vector3 origin;
+
+    void Start()
+    {
+        origin = transform.position;
+    }
+
+    void OnMouseDown()
+    {
+        transform.localScale *= 1.5f;    
+    }
+
+    void OnMouseDrag()
+    {
+        Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        pos.z = 0;
+        transform.position = pos;
+    }
+
+    void OnMouseUp()
+    {
+        transform.localScale = new Vector3(1,1,1);
+        transform.position = origin;
+    }    
+}
